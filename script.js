7f164f8f535f6cf9aff12e8d51893727cc960288
Stanislav <s.rud@factor.corp>
2019-09-03 17:45:29 +0300

    without idea

---
+++ script.js
@@ -0,0 +1,15 @@
+$(function(){
+    $('#date_to').daterangepicker({
+        singleDatePicker: true,
+        locale: {
+            format: 'DD_MM_YYYY'
+        }
+    });
+    $('#date_from').daterangepicker({
+        singleDatePicker: true,
+        autoUpdateInput: false,
+        locale: {
+            cancelLabel: 'Clear'
+        }
+    });
+});
